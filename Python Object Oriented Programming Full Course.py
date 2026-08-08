4b2f828d1c9ee99d558c063e6c01c0496e70d6e0
al.nuriev@yahoo.com <an11@kent.ac.uk>
2026-08-08 15:32:57 +0100

    Continued

--- Python Object Oriented Programming Full Course.py
+++ Python Object Oriented Programming Full Course.py
@@ -240,3 +240,49 @@ square.describe()
 circle.describe()
 
 triangle.describe()
+
+print(end = '\n\n')
+
+# Polymorphism - many forms
+
+from abc import ABC, abstractmethod
+
+class Shape:
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * self.radius ** 2
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Triangle (Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return self.base * self.height * 0.5
+
+class Pizza(Circle):
+    def __init__(self, topping, radius):
+        super().__init__(radius) # calling superclass
+        self.topping = topping
+
+
+
+shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("margarita", 15)]
+
+for shape in shapes:
+    print(f"{shape.area()} cm2")
