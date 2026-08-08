2e878613f9746795d365f9503e6b843e2adeb920
al.nuriev@yahoo.com <an11@kent.ac.uk>
2026-08-08 17:03:50 +0100

    Nexted classes added

--- Python Object Oriented Programming Full Course.py
+++ Python Object Oriented Programming Full Course.py
@@ -371,6 +371,40 @@ car1 = Car('Ford', 'Mustang', 500, 18)
 car2 = Car('Porsche', '911', 800, 15)
 
 print(car1.display_car())
-print(car2.display_car())
+print(car2.display_car(), end = '\n\n')
+
+# nested Classes
+
+class Company:
+    class Employee:
+        def __init__(self, name, position):
+            self.name = name
+            self.position = position
+
+        def get_details(self):
+            return f"{self.name} {self.position}"
+
+    def __init__(self, company_name):
+        self.company_name = company_name
+        self.employees = []
+
+    def add_employee(self, name, position):
+        new_employee = self.Employee(name, position)
+        self.employees.append(new_employee)
+
+    def list_employees(self):
+        return [employee.get_details() for employee in self.employees]
 
 
+company = Company("Krusty Krab")
+company1 = Company('Chum Bucket')
+
+company.add_employee('Eugine', "Manager")
+company.add_employee('Spongebob', "Cook")
+company.add_employee('Squidward', 'Cashier')
+
+company1.add_employee('Sheldon', 'manager')
+company1.add_employee('Karen', 'Assistant')
+
+for employee in company1.list_employees():
+    print(employee)
