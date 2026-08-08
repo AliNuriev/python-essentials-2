314f559042bac09aa580ab243dfd0442e0b5dc7a
al.nuriev@yahoo.com <an11@kent.ac.uk>
2026-08-08 16:20:44 +0100

    Continued

--- Python Object Oriented Programming Full Course.py
+++ Python Object Oriented Programming Full Course.py
@@ -286,3 +286,91 @@ shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("margarita", 15)]
 
 for shape in shapes:
     print(f"{shape.area()} cm2")
+
+print(end='\n\n')
+
+# Duck typing - another method of achieving polymorphism. Object must have the minimum necessary attributes/methods
+
+class Animal:
+    alive = True
+
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+
+class Car:
+
+    alive = False
+
+    def speak(self):
+        print("Honk!")
+
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive, end='\n\n')
+
+# Aggregation - one object contains references to other INDEPENDENT objects "has - a" relation
+
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        return [f"{book.title} by {book.author}" for book in self.books]
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+library = Library("NY public library")
+
+book1 = Book("Harry Potter", "J.K. Rowling")
+book2 = Book("The Hobbit", "J.R.R. Tolkein")
+book3 = Book("The Color of Magic", "Terry Pratchet")
+
+library.add_book(book1)
+library.add_book(book2)
+library.add_book(book3)
+
+print(library.name)
+for book in library.list_books():
+    print(book, end = "\n\n")
+
+# composition - object owns its components, which cannot exist independently
+
+class Engine:
+    def __init__(self, horse_power):
+        self.horse_power = horse_power
+
+class Wheel:
+    def __init__(self, size):
+        self.size = size
+
+class Car:
+    def __init__(self, make, model, horse_power, wheel_size):
+        self.make = make
+        self.model = model
+        self.engine = Engine(horse_power)
+        self.wheels = [Wheel(wheel_size) for wheel in range(4)]
+
+    def display_car(self):
+        return f"{self.make} {self.model}, {self.engine.horse_power}(hp), {self.wheels[0].size}"
+
+car1 = Car('Ford', 'Mustang', 500, 18)
+car2 = Car('Porsche', '911', 800, 15)
+
+print(car1.display_car())
+print(car2.display_car())
+
+
