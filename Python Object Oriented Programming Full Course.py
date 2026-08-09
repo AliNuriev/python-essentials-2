41e72fdd0b04a5a3a2b7a954ee6516615e9f626f
al.nuriev@yahoo.com <an11@kent.ac.uk>
2026-08-09 16:14:50 +0100

    Add magic methods example (Book class)

--- Python Object Oriented Programming Full Course.py
+++ Python Object Oriented Programming Full Course.py
@@ -473,11 +473,57 @@ student2 = Students('Ray', 4.00)
 student3 = Students('Nic', 3.21)
 
 print(Students.get_count())
-print(Students.get_avg_gpa())
+print(Students.get_avg_gpa(), end = '\n\n')
 
+# Magic methods
 
+class Book:
 
+    def __init__(self, title, author, num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
 
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+
+    def __eq__(self, other):
+        return self.title == other.title and self.author == other.author
+
+    def __lt__(self, other):
+        return self.num_pages < other.num_pages
+
+    def __gt__(self, other):
+        return self.num_pages > other.num_pages
+
+    def __add__(self, other):
+        return self.num_pages + other.num_pages
+
+    def __contains__(self, keyword):
+        return keyword in self.title or keyword in self.author
+
+    def __getitem__(self, key):
+
+        if key == 'title':
+            return self.title
+        elif key == 'author':
+            return self.author
+        elif key == 'pages':
+            return self.num_pages
+        else:
+            return f'Key {key} was not found'
+
+book1 = Book("The Hobbit", "J.R.R. Tolkien", 310)
+book2 = Book('Harry Potter', 'J.K. Rowling', 223)
+book3 = Book('Crime and Punishment', 'F. M. Dostoyevski', 345)
+
+print(book1 == book3)
+print(book2 > book3)
+print(book2 + book3)
+print('Crime' in book1)
+print(book1['title'])
+print(book3['author'])
+print(book2['Audio'])
 
 
 
